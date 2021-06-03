13fdc7b699a428e386b758578586ebaa4e190b72
chris <christianie.torres@nmbu.no>
2021-06-03 12:40:46 +0200

    Making tests for animal

--- tests/test_aging.py
+++ tests/test_aging.py
@@ -6,15 +6,29 @@
 __author__ = 'Christianie Torres'
 __email__ = 'christianie.torres@nmbu.no'
 
-from biosim.animals import animal
+from biosim.animals import herbivore
 
 import math
 import random
 import pytest
 
 def test_herbivore_created():
-    h = animal(weight= 5, a = 0)
+    """
+    A test that checks that a herbivore has been created
+    """
+
+    h = herbivore()
     assert h.a == 0
 
-#def test_fitness():
-    #for anim
+def test_herbivore_aging():
+    """
+    A test that checks that the herbivore ages for each year
+    """
+
+    h = herbivore()
+    for n in range(10):
+        h.aging()
+        assert h.a == n + 1
+
+
+
