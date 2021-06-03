a128bb8063c354247995a4b7699e887a7de2fe04
joridholmen <jorid.holmen@nmbu.no>
2021-06-03 09:11:52 +0200

    edited cell class

--- src/biosim/Biome/Cell.py
+++ src/biosim/Biome/Cell.py
@@ -1,61 +1,30 @@
 # -*- coding: utf-8 -*-
 
+
 __author__ = 'Jorid Holmen', 'Christianie Torres'
 __email__ = 'jorid.holmen@nmbu.no', 'christianie.torres@nmbu.no'
 
-
+import random
 
 
 class Cell:
-    '''
+    """
     Class for cells
-    '''
+    """
 
     p = {'f_max': 800.0}
 
     def __init__(self):
-        self._F = self.f_max()
+        self._F = self.p['f_max']
         self.herbivores = []
 
     def adding_animals(self):
-        '''
+        """
         Use of the animal class to add animals to the cell
-        '''
-
-
-
-
-
-
-
-
-
-
+        """
 
 
 class Lowland(Cell):
-    '''
+    """
     subclass for lowlandcells
-    '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    """
