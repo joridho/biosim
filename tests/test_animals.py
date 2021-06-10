6e79ec1ce782c14d9e94880563a01574534e5cad
chris <christianie.torres@nmbu.no>
2021-06-10 20:25:52 +0200

    Edited test functions for animals

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -10,33 +10,42 @@ __email__ = 'christianie.torres@nmbu.no'
 from biosim.animals import Herbivore, Carnivore
 
 def test_parameters():
-    h = Herbivore()
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20})
     assert h.p['w_birth'] == 8.0
 
 def test_herbivore_age():
     """
     A test that checks that a herbivore has been created with age 0
     """
-    h = Herbivore()
-    assert h.age == 0
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20})
+    assert h.age == 5
 
 def test_herbivore_weight():
     '''
         test to check if the herbivore has been given a weight
         '''
-    h = Herbivore()
-    b = h.birth_weight
-    assert h.weight == b
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20})
+    #b = h.birth_weight
+    assert h.weight == 20
 
 def test_herbivore_aging():
     """
     A test that checks that the herbivore ages for each year
     """
-    h = Herbivore()
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 0,
+                       'weight': 20})
     for n in range(10):
         h.aging()
         assert h.age == n + 1
 
+'''
 def test_herbivore_birth_weight():
     """
     A test that checks that the herbivore have been given a birth_weight
@@ -44,12 +53,15 @@ def test_herbivore_birth_weight():
     h = Herbivore()
     birth_w = h.birth_weight
     assert h.birth_weight == birth_w
+'''
 
 def test_herbivore_weight_loss():
     '''
         this is a test for testing if the herbivore looses weight each year
     '''
-    h = Herbivore()
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20})
     current_weight = h.weight
     eta = h.p['eta']
     h.weight_loss()
@@ -59,7 +71,9 @@ def test_herbivore_weight_gain():
     '''
         this is a test for testing if the herbivore gains weight when it eats as much as it wants to
         '''
-    h = Herbivore()
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20})
     current_weight = h.weight
     beta = h.p['beta']
     F = h.p['F']
@@ -72,7 +86,9 @@ def test_herbivore_fitness():
     """
     A test that checks that the herbivore have been given a fitness
     """
-    h = Herbivore()
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20})
     fitness = h.phi
     h.fitness()
     assert fitness == h.phi
@@ -82,14 +98,18 @@ def test_valid_fitness():
     This is a test for checking that the fitness-function returns a phi-value between 0 and 1
     """
     for _ in range(100):
-        h = Herbivore()
+        h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 20})
         assert 0 <= h.phi <= 1
 
 def test_no_newborn_when_mother_weighs_too_little():
     """
     This is a test that checks if the birth probability equals zero when the mother weighs to little.
     """
-    h = Herbivore(weight=3.5, age=3)
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 3,
+                       'weight': 3.5})        #weight=3.5, age=3
     assert h.birth_probability(n=3) ==0
 def test_no_newborn_when_to_few_animals(): #too
     """
@@ -97,7 +117,9 @@ def test_no_newborn_when_to_few_animals(): #too
     a cell.
     """
 
-    h = Herbivore(weight=35, age=3)
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 3,
+                       'weight': 35})                      #weight=35, age=3
     assert h.birth_probability(n=1) == 0
 
 def test_no_newborn_if_newborn_too_fat():
@@ -106,12 +128,16 @@ def test_no_newborn_if_newborn_too_fat():
     than the mother
     """
     # the newborn will weigh more than 15/3.5
-    h = Herbivore(weight=15, age=3)
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 3,
+                       'weight': 15})                          #weight=15, age=3
     assert h.birth_probability(n=3) == 0
 
 def test_birth():
     'This is a test that checks if the Herbivore gives birth when it is supposed to'
-    h = Herbivore(weight=35, age=3)
+    h = Herbivore({'species': 'Herbivore',
+                       'age': 5,
+                       'weight': 35})    #weight=35, age=3
     true = 0
     false = 0
     for _ in range(100):
